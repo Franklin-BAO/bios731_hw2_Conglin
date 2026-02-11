d334664faaacb3804916e567b093ca9a7e14c3a3
Franklin-BAO <baoconglin@outlook.com>
2026-02-11 10:30:57 -0500

    Add files via upload
    Initial commit – BIOS 731 HW2

---
+++ hw2_Final/scripts/00_packages.R
@@ -0,0 +1,11 @@
+suppressPackageStartupMessages({
+  library(here)
+  library(dplyr)
+  library(tidyr)
+  library(purrr)
+  library(ggplot2)
+  library(progressr)
+  
+  library(future)
+  library(future.apply)
+})
---
+++ hw2_Final/scripts/09_helpers.R
@@ -0,0 +1,5 @@
+# scripts/09_helpers.R
+
+get_fig_path <- function(filename) {
+  here("output", "figures", filename)
+}
